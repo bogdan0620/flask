8cddd4c49e6c5d0e0e54da6d11f7e72426f1e0a8
Bogdan <bogdanlargin@gmail.com>
2023-07-18 16:47:44 +0500

    deploy
    Took 29 minutes

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ app.register_blueprint(main_page_bp)
 app.register_blueprint(admin_bp)
 app.register_blueprint(login_bp)
 
+from flask import Blueprint, render_template, request, redirect
+from database.models import add_problem_db, get_all_problems_db
+from main_page import forms
+
+main_page_bp = Blueprint('main_page', __name__)
+
+
+@main_page_bp.route('/')
+def main_page():
+    problems = get_all_problems_db()
+    form = forms.PostProblem()
+    return render_template('index.html', problems=problems, form=form)
+
+
+@main_page_bp.route('/add_problem', methods=['post'])
+def add_question():
+    title = request.form.get('title')
+    message = request.form.get('message')
+    add_problem_db(title=title, message=message)
+    return redirect('/')
 
 @app.route('/profile/<string:name>')
 def get_my_name(name):
